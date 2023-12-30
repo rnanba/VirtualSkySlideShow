308dd855c47e5c3a867e6941625e7736b37fb1e6
R.Nanba <rnanba@gmail.com>
2023-12-30 17:44:54 +0900

    fixed: greasemonkey スクリプトで astrometry.net への画像アップロードができない #7
    - GM.setValue()/getValue() で window 間通信をやるようにした。
      - postMessage() の仕組みは廃止
      - 同一スクリプト間でしか通信できないので astrometry.js, wikipedia.js
        の処理を flickr.js に統合した
    - astrometry.net へのアップロード前に確認ダイアログを出すようにした
      - 間違ってアップロードしてしまう事故防止のため

--- gm_scripts/flickr.js
+++ gm_scripts/flickr.js
@@ -3,7 +3,11 @@
 // @namespace https://rna.hatenablog.com/
 // @version  1.2
 // @include  https://www.flickr.com/photos/*
-// @grant    none
+// @include  https://nova.astrometry.net/*
+// @include   https://ja.wikipedia.org/*
+// @include   https://en.wikipedia.org/*
+// @grant    GM.setValue
+// @grant    GM.getValue
 // ==/UserScript==
 
 window.vsssData = {
@@ -12,8 +16,7 @@ window.vsssData = {
   photos: {}
 };
 
-addEventListener("DOMContentLoaded", function(){
-  
+function flickerScript() {
   const TARGET_SIZE = 2048;
   const DEFAULT_TZ = "+09:00";
 
@@ -174,10 +177,39 @@ addEventListener("DOMContentLoaded", function(){
   abutton.style.cursor = 'pointer';
   abutton.addEventListener('click', function () {
     if (!window.vsssData.astrometryProgress) {
-      window.vsssData.astrometryProgress = true;
-      window.open('http://nova.astrometry.net/upload', 'gs_vsss_astrometry');
+      const photoId = getPhotoId();      
+      window.vsssData.astrometryProgress = photoId;
+      window.vsssData.photos[photoId] = {};
+      const url = getMImageUrl(photoId);
+      (async () => {
+        await GM.setValue('astrometry_result', null);
+        await GM.setValue('upload_image_url', url);
+      })();
+      window.open('https://nova.astrometry.net/upload', 'gs_vsss_astrometry');
       abutton.innerText = '[astrometry: wait...]';
       abutton.style.cursor = 'wait';
+      const getter = async () => {
+        let result = await GM.getValue('astrometry_result');
+        console.log(result);
+        if (result) {
+          if (result == "fail") {
+            abutton.innerText = '[astrometry: failed]';
+            abutton.style.cursor = 'pointer';
+          } else if (result == "cancel") {
+            abutton.innerText = '[astrometry]';
+            abutton.style.cursor = 'pointer';
+          } else {
+            id = window.vsssData.astrometryProgress;
+            abutton.innerText = '[astrometry]';
+            abutton.style.cursor = 'pointer';
+            window.vsssData.photos[id] = result;
+          }
+          window.vsssData.astrometryProgress = null;
+        } else {
+          setTimeout(getter, 1000);
+        } 
+      };
+      setTimeout(getter, 5000);
     }
   });
   document.body.insertBefore(abutton, document.body.firstChild);
@@ -191,9 +223,34 @@ addEventListener("DOMContentLoaded", function(){
   wbutton.style.zIndex = '3000';
   wbutton.style.cursor = 'pointer';
   wbutton.addEventListener('click', function () {
-    window.open('https://ja.wikipedia.org/w/index.php?search', 'gs_vsss_ja_wikipedia');
+    const photoId = getPhotoId();      
+    window.vsssData.wikipediaProgress = photoId;
+    window.vsssData.photos[photoId] = {};
+    const title = getTitle(photoId);
+    console.log(title);
+    (async () => {
+      await GM.setValue('wikipedia_result', null);
+      await GM.setValue('wikipedia_search', title);
+    })();
+    window.open('https://ja.wikipedia.org/w/index.php?search',
+                'gs_vsss_ja_wikipedia');
     wbutton.innerText = '[wikipedia: wait...]';
     wbutton.style.cursor = 'wait';
+    const getter = async () => {
+      let id = null;
+      let result = await GM.getValue('wikipedia_result');
+      console.log(result);
+      if (result) {
+        id = window.vsssData.wikipediaProgress;
+        window.vsssData.wikipediaProgress = null;
+        wbutton.innerText = '[wikipedia]';
+        wbutton.style.cursor = 'pointer';
+        window.vsssData.photos[id] = result;
+      } else {
+        setTimeout(getter, 1000);
+      } 
+    };
+    setTimeout(getter, 5000);
   });
   document.body.insertBefore(wbutton, document.body.firstChild);
 
@@ -209,54 +266,104 @@ addEventListener("DOMContentLoaded", function(){
     }
   }
   setInterval(urlCheck, 200);
-  
-  window.addEventListener("message", function(ev){
-    const photoId = getPhotoId();
-    console.log(ev.data.cmd);
-    switch (ev.data.cmd) {
-    case 'getUrl':
-      window.vsssData.astrometryProgress = photoId;
-      window.vsssData.photos[photoId] = {};
-      const url = getMImageUrl(photoId);
-      console.log(url);
-      ev.source.postMessage(url, "http://nova.astrometry.net");
-      break;
-    case 'getSearch':
-      window.vsssData.wikipediaProgress = photoId;
-      window.vsssData.photos[photoId] = {};
-      const title = getTitle(photoId);
-      console.log(title);
-      ev.source.postMessage(title, "https://ja.wikipedia.org");
-      break;
-    case 'setRADec':
-      console.log(ev.data.ra + " / " + ev.data.dec);
-      let id = null;
-      if (ev.data.src.startsWith("http://nova.astrometry.net/")) {
-        id = window.vsssData.astrometryProgress;
-        window.vsssData.astrometryProgress = null;
-        abutton.innerText = '[astrometry]';
-        abutton.style.cursor = 'pointer';
-      } else if (ev.data.src.startsWith("https://ja.wikipedia.org/") ||
-                 ev.data.src.startsWith("https://en.wikipedia.org/")) {
-        id = window.vsssData.wikipediaProgress;
-        window.vsssData.wikipediaProgress = null;
-        wbutton.innerText = '[wikipedia]';
-        wbutton.style.cursor = 'pointer';
+}
+
+function astrometryScript() {
+  console.log("page = " + location.href);
+  if (location.href.startsWith('https://nova.astrometry.net/upload')) {
+    (async () => {
+      const url = await GM.getValue('upload_image_url');
+      await GM.setValue('upload_image_url', null);
+      console.log("url = " + url);
+      if (url != null) {
+        if (window.confirm("[VSSS] Upload image?")) {
+          document.forms.upload_form.upload_type.value = 'url';
+          document.forms.upload_form.url.value = url;
+          document.forms.upload_form.submit();
+        } else {
+          GM.setValue('astrometry_result', "cancel");
+        }
       }
-      if (id) {
-        window.vsssData.photos[id] = {
-          ra: ev.data.ra,
-          dec: ev.data.dec
-        };
-      }
-      break;
-    case 'astrometryFailed':
-      window.vsssData.astrometryProgress = false;
-      if (ev.data.src.startsWith("http://nova.astrometry.net/")) {
-        abutton.innerText = '[astrometry: failed]';
-        abutton.style.cursor = 'pointer';
-      }
-      break;
+    })();
+  } else if (location.href.startsWith('https://nova.astrometry.net/status/')) {
+    if (document.querySelector("#submission_images div.good")) {
+      console.log("astrometry: ok");
+      location.href =
+        document.querySelector("#submission_images td a[href]").href;
+    } else if (document.querySelector("#submission_images div.bad")) {
+      console.log("astrometry: fail");
+      GM.setValue('astrometry_result', "fail");
     }
-  });
+  } else if (location.href.startsWith('https://nova.astrometry.net/user_images/')) {
+    const ra = document.querySelector("#calibration_table tr + tr td + td").innerText
+    const dec = document.querySelector("#calibration_table tr + tr + tr td + td").innerText
+    GM.setValue('astrometry_result', {
+      ra: ra.replace(/\u00a0+/g, " "),
+      dec: dec.replace(/\u00a0+/g, " ")
+    });
+  }
+}
+
+function wikipediaScript() {
+  console.log("page = " + window.location.href);
+  let raText = null;
+  let decText = null;
+  if (window.location.href.endsWith(".wikipedia.org/w/index.php?search")) {
+    (async () => {
+      const search = await GM.getValue('wikipedia_search');
+      await GM.setValue('wikipedia_search', null);
+      console.log("search = " + search);
+      if (search != null) {
+        document.forms.search.search.value = search;
+      }
+    })();
+    return;
+  } else {
+    const infobox = document.querySelector(".infobox");
+    if (window.location.href.startsWith("https://ja.wikipedia.org/")) {
+      let a = infobox.querySelector("th a[title='赤経']");
+      raText = a.parentNode.parentNode.querySelector("td").textContent;
+      a = infobox.querySelector("th a[title='赤緯']")
+      decText = a.parentNode.parentNode.querySelector("td").textContent;
+    } else if (window.location.href.startsWith("https://en.wikipedia.org/")) {
+      let a = infobox.querySelector("th a[title='Right ascension']");
+      raText = a.parentNode.parentNode.querySelector("td").textContent;
+      a = infobox.querySelector("th a[title='Declination']");
+      decText = a.parentNode.parentNode.querySelector("td").textContent;
+    }
+    if (!raText || !decText) {
+      return;
+    }
+  }
+  const onClick = function () {
+    const RA_REGEXP = /(\d+)h\s*(\d+)m\s(\d+(\.\d+)?)s/;
+    const DEC_REGEXP = /\s*([+-＋−]?\d+)[°º]\s*(\d+)['′]\s*(\d+(\.\d+)?)["″]/;
+    let m = RA_REGEXP.exec(raText);
+    const obj = {};
+    obj.ra = m[1] + "h " + m[2] + "m " + m[3] + "s",
+    m = DEC_REGEXP.exec(decText);
+    let md = m[1].replace('＋', '+').replace('−', '-');
+    obj.dec = md + "° " + m[2] + "' " + m[3] + "\"";
+    console.log(JSON.stringify(obj, null, "  "));
+    GM.setValue('wikipedia_result', obj);
+  };
+  const button = document.createElement('div');
+  button.innerText = '[VSSS]';
+  button.style.color = '#77e';
+  button.style.float = 'right';
+  button.style.cursor = 'copy';
+  button.addEventListener('click', onClick);
+  const h1 = document.querySelector("h1");
+  h1.parentNode.insertBefore(button, h1);
+}
+
+addEventListener("DOMContentLoaded", function() {
+  if (location.href.startsWith('https://www.flickr.com/photos/')) {
+    flickerScript();
+  } else if (location.href.startsWith('https://nova.astrometry.net/')) {
+    astrometryScript();
+  } else if (location.href.startsWith("https://ja.wikipedia.org/") ||
+             location.href.startsWith("https://en.wikipedia.org/")) {
+    wikipediaScript();
+  }
 })
